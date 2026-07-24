6a1f5dd6da3583635b8df1e95a7996370a4bd84c
Sushil <37849020+sushil003@users.noreply.github.com>
2026-07-24 14:18:11 -0500

    Add: valid-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    private boolean isValidBST(TreeNode root,long min, long max){
+        if(root==null){
+            return true;
+        }
+        if(root.val <= min || root.val >= max){
+            return false;
+        }
+        return isValidBST(root.left,min,root.val) && isValidBST(root.right,root.val,max);
+    }
+}
