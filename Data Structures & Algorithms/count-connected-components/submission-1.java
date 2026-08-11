0a63cafb911579e9ed2770fc3ffcf533703e2ad7
Sushil <37849020+sushil003@users.noreply.github.com>
2026-08-11 12:11:42 -0500

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        int component = n;
+        for (int[] edge : edges) {
+            int p1 = find(parent, edge[0]);
+            int p2 = find(parent, edge[1]);
+            if (p1 != p2) {
+                parent[p1] = p2;
+                component--;
+            }
+        }
+        return component;
+    }
+
+    private int find(int[] parent, int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent, parent[i]);
+        }
+        return parent[i];
+    }
+}
